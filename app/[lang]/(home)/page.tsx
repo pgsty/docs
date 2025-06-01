5c1b47b515c6b5f19afc7e9207d725ef9fd175d1
Lantianyou <dtlantianyou@gmail.com>
2025-06-01 10:52:19 +0800

    feat: use cned link

--- app/[lang]/(home)/page.tsx
+++ app/[lang]/(home)/page.tsx
@@ -1,13 +1,14 @@
 import Link from 'next/link';
 
-export default function HomePage() {
+export default async function HomePage(props: { params: Promise<{ lang: string }> }) {
+  const { lang } = await props.params;
   return (
     <main className="flex flex-1 flex-col justify-center text-center">
       <h1 className="mb-4 text-2xl font-bold">Hello World</h1>
       <p className="text-fd-muted-foreground">
         You can open{' '}
         <Link
-          href="/docs/pigsty"
+          href={`/${lang}/docs/pigsty`}
           className="text-fd-foreground font-semibold underline"
         >
           /docs
