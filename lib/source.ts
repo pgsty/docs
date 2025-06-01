d6b0ea0f27c475cacf8658eb152f644e2ee79459
Lantianyou <dtlantianyou@gmail.com>
2025-06-01 11:49:32 +0800

    feat: load icons

--- lib/source.ts
+++ lib/source.ts
@@ -1,6 +1,8 @@
 import { docs } from '@/.source';
 import { loader } from 'fumadocs-core/source';
 import { i18n } from './i18n';
+import { icons } from 'lucide-react';
+import { createElement } from 'react';
 
 // See https://fumadocs.vercel.app/docs/headless/source-api for more info
 export const source = loader({
@@ -8,4 +10,11 @@ export const source = loader({
   i18n,
   baseUrl: '/docs',
   source: docs.toFumadocsSource(),
+  icon(icon) {
+    if (!icon) {
+      // You may set a default icon
+      return;
+    }
+    if (icon in icons) return createElement(icons[icon as keyof typeof icons]);
+  },
 });
