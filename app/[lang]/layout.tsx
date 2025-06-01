a3e042cf149651288171558f83b434751bdd68d7
Lantianyou <dtlantianyou@gmail.com>
2025-06-01 09:59:01 +0800

    feat: hide default locale

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -1,13 +1,12 @@
+import "@/app/global.css"
 import { RootProvider } from 'fumadocs-ui/provider';
 import type { Translations } from 'fumadocs-ui/i18n';
 
-// translations
+
 const cn: Partial<Translations> = {
   search: 'Translated Content',
 };
 
-// available languages that will be displayed on UI
-// make sure `locale` is consistent with your i18n config
 const locales = [
   {
     name: 'English',
--- lib/i18n.ts
+++ lib/i18n.ts
@@ -3,4 +3,5 @@ import type { I18nConfig } from 'fumadocs-core/i18n';
 export const i18n: I18nConfig = {
   defaultLanguage: 'en',
   languages: ['en', 'cn'],
+  hideLocale: "default-locale",
 };
